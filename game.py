10d3df2743318e8b1e4cff5a32e137d3fd03715b
Davin Bentti <bentti@gmail.com>
2011-04-16 16:13:05 -0400

    Bug fixes for diagonals with unit tests.  Simplified tests for wins.

---
+++ game.py
@@ -0,0 +1,36 @@
+from strategy import C4Easy
+from board import ConnectFourBoard
+__author__ = 'Davin'
+
+def gameRunner():
+    c4Board = ConnectFourBoard()
+    c4Strategy = C4Easy()
+
+    print " Welcome to Python Connect Four"
+    print " Enter a column to make a move..."
+    c4Board.prettyPrint()
+
+    while True:
+        move = raw_input("Make your move (q to quit): ")
+
+        if move != 'q':
+            if (c4Board.makeMove( int(move), ConnectFourBoard.playerMove)):
+                if (c4Board.hasWinner()):
+                    c4Board.prettyPrint()
+                    print "YOU WON!"
+                    break
+            else:
+                print "BAD MOVE"
+        else:
+            print "Game terminated."
+            break
+        #Computer's turn
+        c4Strategy.computeMove(c4Board)
+        c4Board.prettyPrint()
+        if (c4Board.hasWinner()):
+            print "COMPUTER WON!"
+            break
+
+if __name__ == "__main__":
+    gameRunner()
+
